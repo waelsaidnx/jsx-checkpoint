805b588e16910b8766735712863ab9f014d3471b
Wael SAID <wael.said@test.com>
2021-10-03 12:58:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import image from './imageInSrc.jpg';
+import './style.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div style={{border: "solid",borderWidth:"1px", color:"#6497b1",maxWidth:"100vw"}}>
+
+<h1 className="title red">Wael SAID</h1>
+
+<br />
+
+<img src= {image} />
+
+<br />
+
+<img src="/imageInPublic.jpg" />
+
+</div>
+
+<video style={{width:"320px", height:"240px"}} controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+</video>
+
     </div>
   );
 }
